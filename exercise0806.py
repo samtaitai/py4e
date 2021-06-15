3945b22209904a7bf2a9456edcf565dd64f7550b
Soyon <samtaitai@gmail.com>
2021-06-15 12:23:51 -0400

    'add_note_again'

--- exercise0806.py
+++ exercise0806.py
@@ -1,9 +1,12 @@
 numlist = []
 submit = input('Enter a number: ')
+#stop and exit program
 while True:
     submit == 'Done'
     break
+#add valuables into the list
 else:
     numlist.append(submit)
+#max and min methods for list
 print('Maximum:', max(numlist))
 print('Minimum:', min(numlist))
